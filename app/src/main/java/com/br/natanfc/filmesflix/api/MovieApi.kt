d9376e9624232601f3e03d69bc446ec28710cf7b
tonin <antonio.leite00@hotmail.com>
2021-08-27 23:25:53 -0300

    Criado interface e preparada rota para consumir API

---
+++ app/src/main/java/com/br/natanfc/filmesflix/api/MovieApi.kt
@@ -0,0 +1,14 @@
+package com.br.natanfc.filmesflix.api
+
+import com.br.natanfc.filmesflix.model.Movie
+import retrofit2.http.GET
+
+interface MovieApi {
+
+    //https://raw.githubusercontent.com/natanfelipe/FilmesFlixJson/master/moviesList
+
+
+    @GET("natanfelipe/FilmesFlixJson/master/moviesList")
+    fun getAllMovies (): List<Movie>
+
+}
